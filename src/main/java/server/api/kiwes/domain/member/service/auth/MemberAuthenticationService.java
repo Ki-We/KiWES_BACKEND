d6f119beb29325f97657d6d6d90249c7cabb28f3
Seungwoo, Joo <thisis.joos@gmail.com>
2023-08-07 20:44:18 +0900

    hotfix: kakao redirect url

--- src/main/java/server/api/kiwes/domain/member/service/auth/MemberAuthenticationService.java
+++ src/main/java/server/api/kiwes/domain/member/service/auth/MemberAuthenticationService.java
@@ -237,7 +237,7 @@ public class MemberAuthenticationService {
                 StringBuilder sb = new StringBuilder();
                 sb.append("grant_type=authorization_code");
                 sb.append("&client_id=93df5ea9a1445313343f4bb0f1d362ce"); // TODO REST_API_KEY 입력
-                sb.append("&redirect_uri=http://localhost:8080/oauth/kakao"); // TODO 인가코드 받은 redirect_uri 입력
+                sb.append("&redirect_uri=http://43.200.185.205:8080/oauth/kakao"); // TODO 인가코드 받은 redirect_uri 입력
                 sb.append("&code=" + code);
                 bw.write(sb.toString());
                 bw.flush();
